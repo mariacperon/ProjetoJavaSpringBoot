f194e4a55cfd19a4b677c135436d3aec7cf97be0
mariacperon <mariaclaraperon@gmail.com>
2021-12-23 13:36:25 -0300

    projeto criado

---
+++ src/main/java/com/project/ProjetoCursoSP/ProjetoCursoSpApplication.java
@@ -0,0 +1,13 @@
+package com.project.ProjetoCursoSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ProjetoCursoSP/ProjetoCursoSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ProjetoCursoSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
