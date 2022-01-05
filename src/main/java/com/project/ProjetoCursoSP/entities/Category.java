5a48ddd560fcad06c664fb6da76e856716d6256e
mariacperon <mariaclaraperon@gmail.com>
2022-01-05 14:05:26 -0300

    jointable category&product

--- src/main/java/com/project/ProjetoCursoSP/entities/Category.java
+++ src/main/java/com/project/ProjetoCursoSP/entities/Category.java
@@ -1,9 +1,12 @@
 package com.project.ProjetoCursoSP.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import java.io.Serializable;
@@ -20,7 +23,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
