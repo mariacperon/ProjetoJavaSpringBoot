6404e607bbcbb1b0334761206df03c5891af8790
mariacperon <mariaclaraperon@gmail.com>
2022-01-05 15:11:55 -0300

    subtotal & tal methods

--- src/main/java/com/project/ProjetoCursoSP/entities/Order.java
+++ src/main/java/com/project/ProjetoCursoSP/entities/Order.java
@@ -100,6 +100,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal(){
+        double total = 0;
+        for(OrderItem item : items){
+            total += item.getSubTotal();
+        }
+        return total;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/main/java/com/project/ProjetoCursoSP/entities/Payment.java
+++ src/main/java/com/project/ProjetoCursoSP/entities/Payment.java
@@ -1,5 +1,7 @@
 package com.project.ProjetoCursoSP.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,6 +22,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
